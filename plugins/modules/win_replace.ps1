08cf7d7651306f21aa56cbc363d22ab0bde6150c
idan-tankel <shakel002@gmail.com>
2021-04-12 21:32:19 +0300

    first commit

--- plugins/modules/win_replace.ps1
+++ plugins/modules/win_replace.ps1
@@ -4,3 +4,10 @@
 
 #Requires -Module Ansible.ModuleUtils.Legacy
 #Requires -Module Ansible.ModuleUtils.Backup
+
+
+## initialize ansible parameters (user inputs)
+# The user inputs here reflect the inputs given in ansible original replace module
+
+$path = Get-AnsibleParam -obj $params -name "path" -type "path" -failifempty $true -aliases "dest","destfile","name";
+$regexp = Get-AnsibleParam -obj $params -name "regex" -type "str" -aliases "regex";
